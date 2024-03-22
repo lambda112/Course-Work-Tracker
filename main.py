2b0bac41fcdfcf6ffc092ee0fc30ec0565863f4f
Kamran Azim <Kamazim121212@gmail.com>
2024-03-22 00:50:43 +0000

    User is able to tick using checkbutton courswork pieces they have completed. Then using a button they can found how many pieces of work they have completed

--- main.py
+++ main.py
@@ -13,21 +13,24 @@ unit22_label = Label(text = "UNIT 22 - Big Data Analytics ").grid(row=4, column=
 unit23_label = Label(text = "UNIT 23 - Cognitive Computing").grid(row=5, column=0)
 unit24_label = Label(text = "UNIT 24 - Enterprise Computing").grid(row=6, column=0)
 
+total_label = Label(text = "Total: 0")
+total_label.grid(column=12, row=7)
+
 # Tasks
 unit7_tasks = ["P1", "P2", "P3", "P4", "P5", "P6", "M1", "M2", "M3", "D1", "D2"]
 unit10_tasks = ["P1", "P2", "P3", "P4", "P5","M1", "M2", "M3", "D1", "D2"]
 unit23_tasks = ["P1", "P2", "P3", "M1", "M2", "D1"]
 
-# Function
-def create_checkbox(unit_list:list = [], column_val:int = 1, row_val:int = 0, new_list:list = []) -> list:
-    for task in unit_list:
-        unit_check = Checkbutton(text = task).grid(row=row_val, column=column_val)
-        new_list.append(unit_check)
+# Checkbox
+def create_checkbox(unit_list:list = [], column_val:int = 1, row_val:int = 0) -> list:
+    new_list = []
+    for index, task in enumerate(unit_list):
+        new_list.append(IntVar())
+        Checkbutton(text = task, offvalue = 0, onvalue = 1, variable=new_list[index]).grid(row=row_val, column=column_val, sticky="ew")
         column_val += 1 
     
     return new_list
 
-# Checkbox
 unit7 = create_checkbox(unit_list = unit7_tasks, column_val=1, row_val=0)
 unit9 = create_checkbox(unit_list = unit7_tasks, column_val=1, row_val=1)
 unit10 = create_checkbox(unit_list = unit10_tasks, column_val=1, row_val=2)
@@ -35,5 +38,18 @@ unit12 = create_checkbox(unit_list = unit7_tasks, column_val=1, row_val=3)
 unit22 = create_checkbox(unit_list = unit7_tasks, column_val=1, row_val=4)
 unit23 = create_checkbox(unit_list = unit23_tasks, column_val=1, row_val=5)
 unit24 = create_checkbox(unit_list = unit10_tasks, column_val=1, row_val=6)
+all_work = unit7 + unit9 + unit10 + unit12 + unit22 + unit23 + unit24
+
+# Button
+def update_total():
+    total = 0
+    for unit in all_work:
+        print(unit.get())
+        if unit.get():
+            total+=1
+
+    total_label.config(text = f"Total: {total}")
+    
+done_button = Button(text = "Calculate Total", command=update_total).grid(row=7, column=11)
 
 root.mainloop()
